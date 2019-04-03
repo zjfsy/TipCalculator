b46003f9c08d5e60af63324852e1b43fa9129472
ZJF <zjftd@tju.edu.cn>
2019-04-03 19:05:14 +0800

    Homework3

---
+++ TipCalculator/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TipCalculator
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Tip tip;
+        public MainWindow()
+        {
+            InitializeComponent();
+            tip = new Tip();
+        }
+
+        private void AmountLostFocus(object sender, RoutedEventArgs e)
+        {
+            //判断输入是否有效并将TextBox的值转为标准的金钱格式
+            if (!double.TryParse(amount.Text, out double bill))
+            {
+                bill = 0;
+            }
+            amount.Text = string.Format("{0:C}", bill);
+            Calculate();
+        }
+
+        private void AmountGotFocus(object sender, RoutedEventArgs e)
+        {
+            //将TextBox的值置空
+            amount.Text = string.Empty;
+        }
+
+        private void ClickRadio(object sender, RoutedEventArgs e)
+        {
+            Calculate();
+        }
+
+        private new void MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            //当鼠标离开TextBox范围时使TextBox失去焦点并计算Tip
+            vb.Focus();
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            RadioButton selectedRadio = stackPanel.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked == true);
+            tip.CalculateTip(amount.Text, double.Parse(selectedRadio.Tag.ToString()));
+            tips.Text = tip.TipAmount;
+            total.Text = tip.TotalAmount;
+        }
+    }
+}
---
+++ TipCalculator/Tip.cs
@@ -0,0 +1,29 @@
+﻿namespace TipCalculator
+{
+    public class Tip
+    {
+        public Tip()
+        {
+            BillAmount = string.Empty;
+            TipAmount = string.Empty;
+            TotalAmount = string.Empty;
+        }
+        public void CalculateTip(string amount, double percentage)
+        {
+            double billAmount = 0.0;
+            double tipAmount = 0.0;
+            double totalAmount = 0.0;
+            if (double.TryParse(amount.Substring(1), out billAmount))
+            {
+                tipAmount = billAmount * percentage;
+                totalAmount = billAmount + tipAmount;
+            }
+            BillAmount = string.Format("{0:C}", billAmount);
+            TipAmount = string.Format("{0:C}", tipAmount);
+            TotalAmount = string.Format("{0:C}", totalAmount);
+        }
+        public string BillAmount { get; set; }
+        public string TipAmount { get; set; }
+        public string TotalAmount { get; set; }
+    }
+}
